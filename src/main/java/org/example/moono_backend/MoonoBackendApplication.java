762b42d2c27e46ac4092bd41fc898befc2e210a1
dnwldla <leeemjw@naver.com>
2026-01-07 16:13:20 +0900

    first commit

---
+++ src/main/java/org/example/moono_backend/MoonoBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.moono_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonoBackendApplication.class, args);
+    }
+
+}
