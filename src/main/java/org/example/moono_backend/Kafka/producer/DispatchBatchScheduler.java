7d36981af5d4053e229ad7bd7d304c0f0baf99da
yubin012 <101037608+yubin012@users.noreply.github.com>
2026-01-16 14:07:21 +0900

    프로젝트 구조 변경

---
+++ src/main/java/org/example/moono_backend/Kafka/producer/DispatchBatchScheduler.java
@@ -0,0 +1,10 @@
+package org.example.moono_backend.Kafka.producer;
+
+import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class DispatchBatchScheduler {
+
+}
