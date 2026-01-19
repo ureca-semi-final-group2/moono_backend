5a364bb6c6c2486d4ea4e5ef6c2a0ffb65d53f32
dnwldla <leeemjw@naver.com>
2026-01-19 17:06:49 +0900

    [Chore] 클래스명을 FixedDiscountPolicy ->DiscountPolicy로 수정

--- src/main/java/org/example/moono_backend/dto/DiscountInfo.java
+++ src/main/java/org/example/moono_backend/dto/DiscountInfo.java
@@ -1,17 +1,17 @@
 package org.example.moono_backend.dto;
 
-import org.example.moono_backend.domain.discount.FixedDiscountPolicy;
+import org.example.moono_backend.domain.discount.DiscountPolicy;
 
 public record DiscountInfo(
         String code, // 코드
         String discountName, // 표시명
         int discountAmount // 할인 금액
 ) {
-    public static DiscountInfo from(FixedDiscountPolicy fixedDiscountPolicy, int originalPrice) {
+    public static DiscountInfo from(DiscountPolicy discountPolicy, int originalPrice) {
         return new DiscountInfo(
-                fixedDiscountPolicy.name(),
-                fixedDiscountPolicy.getDisplayName(),
-                fixedDiscountPolicy.discountAmount(originalPrice)
+                discountPolicy.name(),
+                discountPolicy.getDisplayName(),
+                discountPolicy.discountAmount(originalPrice)
         );
     }
 }
