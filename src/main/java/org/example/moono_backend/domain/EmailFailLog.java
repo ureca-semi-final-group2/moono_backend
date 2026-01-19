50a004aab701f0d10b59da846c1fbfc6acd475a7
HoonSeok Choi <gnstjrdlsla18@gmail.com>
2026-01-19 23:59:36 +0900

    [Feat] 카프카 DLT Consumer 서비스 구현

--- src/main/java/org/example/moono_backend/domain/EmailFailLog.java
+++ src/main/java/org/example/moono_backend/domain/EmailFailLog.java
@@ -1,15 +1,12 @@
 package org.example.moono_backend.domain;
 
-import org.example.moono_backend.domain.member.MemberCredential;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EmailFailLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,5 +17,17 @@ public class EmailFailLog {
     @Enumerated(EnumType.STRING)
     private SmsSendStatus smsStatus;
 
+    @Column(length = 1024)
     private String payload; // 카프카 토픽에 담겼던 JSON 데이터 통째로 저장
+
+    @Enumerated(EnumType.STRING)
+    private ParseStatus parseStatus; // JSON 파싱 성공 여부
+
+    @Builder
+    public EmailFailLog(String publicInfoId, String payload, ParseStatus parseStatus) {
+        this.publicInfoId = publicInfoId;
+        this.smsStatus = SmsSendStatus.PENDING;
+        this.payload = payload;
+        this.parseStatus = parseStatus;
+    }
 }
